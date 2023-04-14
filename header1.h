bb55207226232753dbb5cf3f1dc3b3da2403ba9e
westpearl <129414017+020728jinju@users.noreply.github.com>
2023-04-14 13:24:58 +0900

    git test

---
+++ header1.h
@@ -0,0 +1,11 @@
+void lcm1(void); // 파라미터 없는 최소공배수 함수
+
+int lcm2(int num1, int num2); // 정수 자료형을 파라미터로 하는 최소공배수 함수
+
+void gcd1(void); // 파라미터 없는 최대공약수 함수
+
+int gcd2(int num3, int num4); // 정수 자료형을 파라미터로 하는 최소공약수 함수
+
+void pnt1(void); // 파라미터 없는 소수 판별 함수
+
+int pnt2(int num5); // 정수 자료형을 파라미터로 하는 소수 판별 함수
